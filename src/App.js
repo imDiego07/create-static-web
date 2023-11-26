6a15127effdc18706dbc640c09479769b9f00f48
imDiego07 <yeyo.spider@gmail.com>
2023-11-26 03:39:02 -0600

    codigo 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
+import React, { useState } from 'react';
+import './App.css'; // Puedes personalizar el estilo en este archivo si decides crearlo.
+
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // Estados para almacenar la URL de la imagen y el mensaje de la imagen
+  const [imageUrl, setImageUrl] = useState(''); // Estado para la URL de la imagen
+  const [imageMessage, setImageMessage] = useState(''); // Estado para el mensaje de la imagen
+
+  // Función para manejar el análisis de la imagen (por implementar)
+  const handleImageAnalysis = () => {
+    // Implementar la lógica de análisis de imagen aquí
+    // Puedes utilizar los valores de imageUrl e imageMessage
+    console.log('Análisis de Imagen');
+  };
+
+  // Función para manejar la generación de la imagen (por implementar)
+  const handleImageGeneration = () => {
+    // Implementar la lógica de generación de imagen aquí
+    // Puedes utilizar los valores de imageUrl e imageMessage
+    console.log('Generación de Imagen');
+  };
+
+  return (
+    <div className="app">
+      <h1>Aplicación de Análisis de Imágenes</h1>
+
+      <div>
+        <label>URL de la Imagen:</label>
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Mensaje de la Imagen:</label>
+        <input
+          type="text"
+          value={imageMessage}
+          onChange={(e) => setImageMessage(e.target.value)}
+        />
+      </div>
+
+      <button onClick={handleImageAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
-
-//prueba de commit
-
-
